7e9ce043d98b915fca664740f6f22e475b57ddee
Lorentz Lasson <lorentz.lasson@se.ibm.com>
2015-03-24 16:02:44 +0100

    supports cloudant

--- src/net/bluemix/iot/bussimulator/data/DataLayer.java
+++ src/net/bluemix/iot/bussimulator/data/DataLayer.java
@@ -1,96 +1,66 @@
 package net.bluemix.iot.bussimulator.data;
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.security.InvalidParameterException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 
-import net.bluemix.iot.bussimulator.Main;
 import net.bluemix.iot.bussimulator.model.BusRoute;
 import net.bluemix.iot.bussimulator.model.Coordinate;
 import net.bluemix.iot.bussimulator.util.CoordGenerator;
 import net.bluemix.iot.bussimulator.util.Util;
 
+import com.cloudant.client.api.CloudantClient;
+import com.cloudant.client.api.Database;
+
 public class DataLayer {
 
-	private static Map<String, BusRoute> routes;
+	private static Map<String, BusRoute> routes = new HashMap<String, BusRoute>();
 
-	public static void initialize(){
+	public static void initializeFromCsv(){
 
-		routes = new HashMap<String, BusRoute>();
 		BusRoute busRoute;
 
 		// Bus number 1
 		busRoute = new BusRoute(
 				"1",
-				coordinatesFromCsv("/line1.csv"), 
+				Util.coordinatesFromCsv("/line1.csv"), 
 				true);
 		routes.put(busRoute.getNumber(), busRoute);
 
 		// Bus number 2
 		busRoute = new BusRoute(
 				"2",
-				coordinatesFromCsv("/line2.csv"), 
+				Util.coordinatesFromCsv("/line2.csv"), 
 				true);
-
 		routes.put(busRoute.getNumber(), busRoute);
 		
 		// Bus number 3
 		busRoute = new BusRoute(
 				"3",
-				coordinatesFromCrossRoadsCsv("/line3cr.csv"),
+				Util.coordinatesFromCsv("/line3cr.csv"),
 				false);
 
 		routes.put(busRoute.getNumber(), busRoute);
 	}
-
-	public static BusRoute getRoute(String number){
-		return routes.get(number);
-	}
-
-	public static BusRoute getRandomRoute(){
-		Random random = new Random();
-		int randomIndex = random.nextInt(routes.keySet().size() - 1);
-		BusRoute randomRoute = null;
-
-		int i = 0;
-		for (BusRoute route : routes.values()) {
-			if (i == randomIndex) randomRoute = route;
-			i++;
-		}
-		return randomRoute;
-	}
-
-	private static Coordinate[] coordinatesFromCsv(String path){
-		BufferedReader br = null;
-		String line = "";
-		String cvsSplitBy = ",";		
-		List<Coordinate> coordinateList = new ArrayList<Coordinate>();
-		InputStream is = Main.class.getResourceAsStream(path);
-		InputStreamReader isr = new InputStreamReader(is);
-		try {
-			br = new BufferedReader(isr);
-			while ((line = br.readLine()) != null) {
-				// use comma as separator
-				String[] entry = line.split(cvsSplitBy);
-				double latitude = Double.parseDouble(entry[0]);
-				double longitude = Double.parseDouble(entry[1]);
-				coordinateList.add(new Coordinate(latitude, longitude));
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		Coordinate[] coordinates = coordinateList.toArray(new Coordinate[coordinateList.size()]);
-		return coordinates;
-	}
 	
-	private static Coordinate[] coordinatesFromCrossRoadsCsv(String path){
-		Coordinate[] checkPoints = coordinatesFromCsv(path);
-		List<Coordinate> listPath = CoordGenerator.createPathFromCheckpoints(checkPoints, 1);
-		Coordinate[] arrPath = Util.listToArray(listPath);
-		return arrPath;				
+	public static void initializeFromCloudant(){
+		CloudantClient client = new CloudantClient(
+				"https://1505a557-0dfb-47ee-bbdd-b2cf1e770c65-bluemix:fc43906f40140a60d8b37c6e3e36bbb4b6ffcc97615b2c62e425a26d703df8eb@1505a557-0dfb-47ee-bbdd-b2cf1e770c65-bluemix.cloudant.com",
+				"1505a557-0dfb-47ee-bbdd-b2cf1e770c65-bluemix",
+				"fc43906f40140a60d8b37c6e3e36bbb4b6ffcc97615b2c62e425a26d703df8eb");
+
+		Database db = client.database("bus_routes", false);
+		List<BusRoute> busRoutes = db.view("_all_docs").includeDocs(true).query(BusRoute.class);
+		for (BusRoute busRoute : busRoutes) {
+			List<Coordinate> path = CoordGenerator.createPathFromCheckpoints(busRoute.getCoordinates(), 2);
+			busRoute.setCoordinates(Util.listToArray(path));
+			routes.put(busRoute.getNumber(), busRoute);
+		}
+	}
+
+	public static BusRoute getRoute(String number) throws InvalidParameterException{
+		BusRoute busRoute = routes.get(number);
+		if (busRoute == null) throw new InvalidParameterException("Route "+number+" does not exist");
+		return busRoute;
 	}
 }
