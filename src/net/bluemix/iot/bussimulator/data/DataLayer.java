e19aec80a59f5d7eb1482f3d03a789e7c49e55eb
Lorentz Lasson <lorentz.lasson@se.ibm.com>
2015-03-30 16:01:12 +0200

    removed bus register query, done towards IoTF instead

--- src/net/bluemix/iot/bussimulator/data/DataLayer.java
+++ src/net/bluemix/iot/bussimulator/data/DataLayer.java
@@ -1,51 +1,26 @@
 package net.bluemix.iot.bussimulator.data;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.security.InvalidParameterException;
 import java.util.List;
-import java.util.Properties;
 
 import net.bluemix.iot.bussimulator.BusSimulator;
 import net.bluemix.iot.bussimulator.model.BusRoute;
-import net.bluemix.iot.bussimulator.util.Util;
-
-import org.apache.commons.io.IOUtils;
 
 import com.cloudant.client.api.CloudantClient;
 import com.cloudant.client.api.Database;
 import com.cloudant.client.api.model.SearchResult;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
 public class DataLayer {
 
 	private Database db;
 
 	public DataLayer() {
-
-
 		CloudantClient client = new CloudantClient(
 				BusSimulator.cloudantCredentials.getProperty("url"),
 				BusSimulator.cloudantCredentials.getProperty("username"),
 				BusSimulator.cloudantCredentials.getProperty("password"));
 
 		db = client.database("bus_routes", false);
-	}
-
-	public JsonArray getBusRegister() {
-		InputStream inStream = db.find("busCounter");
-		String busRegister = null;
-		try {
-			busRegister = IOUtils.toString(inStream);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		JsonObject jsonObject = new JsonParser().parse(busRegister).getAsJsonObject();
-		JsonArray jsonArray = jsonObject.get("busCounts").getAsJsonArray();
-		return jsonArray;
 	}
 
 	public void saveRoute(BusRoute busRoute){
@@ -65,6 +40,4 @@ public class DataLayer {
 		BusRoute busRoute = result.get(0).getDoc();
 		return busRoute;
 	}
-
-
 }
