dcf0b79380ff2655d22c4f77dce2f330990349e0
Lorentz Lasson <lorentz.lasson@se.ibm.com>
2015-03-31 13:49:04 +0200

    simulated events for user and sensor on different intervals

--- src/net/bluemix/iot/bussimulator/BusSimulator.java
+++ src/net/bluemix/iot/bussimulator/BusSimulator.java
@@ -4,14 +4,17 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 import java.util.Properties;
+import java.util.Random;
 import java.util.concurrent.CopyOnWriteArrayList;
 
-import net.bluemix.iot.bussimulator.connect.MqttLayer;
-import net.bluemix.iot.bussimulator.connect.RestLayer;
+import net.bluemix.iot.bussimulator.connection.MqttLayer;
+import net.bluemix.iot.bussimulator.connection.RestLayer;
 import net.bluemix.iot.bussimulator.data.DataLayer;
 import net.bluemix.iot.bussimulator.exception.BusSimulatorException;
 import net.bluemix.iot.bussimulator.model.Bus;
 import net.bluemix.iot.bussimulator.model.BusRoute;
+import net.bluemix.iot.bussimulator.model.softhouse.SensorEvent;
+import net.bluemix.iot.bussimulator.model.softhouse.UserEvent;
 import net.bluemix.iot.bussimulator.util.Util;
 
 import com.google.gson.JsonArray;
@@ -19,8 +22,12 @@ import com.google.gson.JsonObject;
 
 public class BusSimulator{
 
-	private static final long INTERVAL_UPDATE = 1000; // 1 second
-
+	private static final long INTERVAL_POSITION = 1000;	// 1  sec
+	private static final long INTERVAL_USER		= 30000;// 30 sec
+	private static final long INTERVAL_SENSOR	= 10000;// 10 sec
+	private long lastUserEvent					= 0;
+	private long lastSensorEvent				= 0;
+	
 	public static Properties iotfCredentials;
 	public static Properties cloudantCredentials;
 
@@ -57,6 +64,7 @@ public class BusSimulator{
 
 	public void addBus(String number) {
 		String id = restLayer.registerNewBus(number);
+		
 		BusRoute busRoute;
 		try {
 			busRoute = dataLayer.getRoute(number);
@@ -68,19 +76,22 @@ public class BusSimulator{
 	}
 
 	public void startBuses() {
-//		int count = 0;
 		while (true) {
-			// TODO for testing
-//			count++;
-//			if (count % 5 == 0) {
-//				mqtt.addNewBus();
-//			}
-
 			moveBuses();			
-			publishBuses();
-
+//			publishBusPositions();
+			
+			long now = System.currentTimeMillis();
+			if (now - lastSensorEvent > INTERVAL_SENSOR) {
+				publishBusSensors();
+				lastSensorEvent = now;
+			}
+			if (now - lastUserEvent > INTERVAL_USER) {
+				publishBusUsers();
+				lastUserEvent = now;
+			}
+			
 			try {
-				Thread.sleep(INTERVAL_UPDATE);
+				Thread.sleep(INTERVAL_POSITION);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
@@ -93,9 +104,32 @@ public class BusSimulator{
 		}
 	}
 
-	private void publishBuses(){
+	private void publishBusPositions(){
 		for (Bus bus : buses) {
-			mqtt.publishBusMovement(bus);
+			mqtt.publishPosition(bus);
+		}
+	}
+	
+	private void publishBusUsers(){
+		Random random = new Random();
+		for (Bus bus : buses) {
+			UserEvent event = new UserEvent(bus);
+			int id = 1 + random.nextInt(3);
+			event.setId(String.valueOf(id));
+			if (random.nextBoolean()) event.setTrip("start");
+			else event.setTrip("end");
+			mqtt.publishUser(event);
+		}
+	}
+	
+	private void publishBusSensors(){
+		Random random = new Random();
+		for (Bus bus : buses) {
+			SensorEvent event = new SensorEvent(bus);
+			event.setId("5");
+			double temprature = 20 + random.nextDouble() * 5;
+			event.setValue(String.format("%.2f", temprature));
+			mqtt.publishSensor(event);
 		}
 	}
 
