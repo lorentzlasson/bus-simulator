52e321b351ed0a9c4f23e87a5bd9516d05c8844d
Lorentz Lasson <lorentz.lasson@se.ibm.com>
2015-04-07 14:58:54 +0200

    get set id

--- src/net/bluemix/iot/bussimulator/model/Sensor.java
+++ src/net/bluemix/iot/bussimulator/model/Sensor.java
@@ -3,5 +3,12 @@ package net.bluemix.iot.bussimulator.model;
 public class Sensor {
 	
 	private String id;
-	
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
 }
